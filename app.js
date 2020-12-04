4dfb10b65939fa13bc2df62e41db0c203c847eb7
Jefferson Estacio <jestacios@est.ups.edu.ec>
2020-12-04 13:57:54 -0500

    Commint 3

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
-const csv = require('csv-parser');
-const fs = require('fs');
+const datos = require('./controlador/datos');
 
-fs.createReadStream('datos.csv')
-    .pipe(csv())
-    .on('data', (row) => {
-        console.log(row);
-    })
-    .on('end', () => {
-        console.log('CSV file successfully processed');
+const argv = require('yargs').options({
+    archivo: {
+        alias: 'f',
+        desc: 'Permite establecer el path del archivo CSV que contiene los datos a analizar',
+        demand: true
+    },
+    pais: {
+        default: true,
+        alias: 'c',
+        desc: 'Permite determinar el país a analizar a través de su código ISO 3166 ALPHA-3.',
+    },
+    anio: {
+        default: true,
+        alias: 'y',
+        desc: 'Permite especificar el año para el cual se requiere las estadísticas.',
+    }
+
+}).argv;
+
+let pais = argv.pais
+let anio = argv.anio
+
+datos.getDatos(pais, anio)
+    .then(respuesta => {
+        console.log(`El codigo equivale al pais ${pais} y el año es ${anio}`);
+    }).catch(err => {
+        console.log(err);
     });
